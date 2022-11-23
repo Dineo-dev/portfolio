0faabe5072670c6df0e949ae5ba881b84d017303
Dineo <dineo.phahla@dadevs.co.za>
2022-11-23 14:41:31 +0200

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import App from './App'
+import About from './components/about/about'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
     <>
+      <Header/>
+      <Navbar/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
     </>
   )
 }
